602d9e2a7e2ef53e54d96df6a9561cc3cfaffff3
Alex Alonso <alex.xmde@gmail.com>
2016-04-24 18:40:25 -0300

    Homepage

--- html/index.php
+++ html/index.php
@@ -1,9 +1,94 @@
 <?php include 'templates/header.html'; ?>
 <article class="container">
 	<div class="row">
+		<ul class="articles-list">
+			<li class="item mobile col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header">
+						<h1><a href="#">Mobile</a></h1>
+					</header>
+					<article class="item-title">
+						<h2><a href="#">Android 6.1 - O que mudou? O que mudará?
+							E mais um titulo que é muito grande e ficará escondido
+</a>
+</h2>
+					</article>
+					<footer>
+					 por: <span><a href="/quem-somos/#eclair">Eclair Da Galera</a></span>
+					</footer>
+				</div>
+			</li>
+		<li class="item js col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header">
+						<h1><a href="#">JavaScript</a></h1>
+					</header>
+					<article class="item-title">
+						<h2><a href="#">ES2015 - Um novo caminho para o JavaScript</a></h2>
+					</article>
+					<footer>
+					 por: <span><a href="/quem-somos/#alex">Alex Alonso</a></span>
+					</footer>
+				</div>
+			</li>
+		<li class="item html col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header">
+						<h1><a href="#">HTML e CSS</a></h1>
+					</header>
+					<article class="item-title">
+						<h2><a href="#">(Tutorial) Game em HTML5 utilizando Canvas</a></h2>
+					</article>
+					<footer>
+					 por: <span><a href="/quem-somos/#alex">Guilherme Felipe</a></span>
+					</footer>
+				</div>
+			</li>
+		<li class="item design col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header">
+						<h1><a href="#">Design e UX</a></h1>
+					</header>
+					<article class="item-title">
+						<h2><a href="#">(Artigo) Entenda de uma vez por todas o que é UX</a></h2>
+					</article>
+					<footer>
+					 por: <span><a href="/quem-somos/#amanda">Amanda Altava</a></span>
+					</footer>
+				</div>
+			</li>
+		<li class="item design col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header">
+						<h1><a href="#">Design e UX</a></h1>
+					</header>
+					<article class="item-title">
+						<h2><a href="#">Flat vs Realismo</a></h2>
+					</article>
+					<footer>
+					 por: <span><a href="/quem-somos/#amanda">Amanda Altava</a></span>
+					</footer>
+				</div>
+			</li>
+		<li class="item js col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header">
+						<h1><a href="#">JavaScript</a></h1>
+					</header>
+					<article class="item-title">
+						<h2><a href="#">TypeScript, CoffeeScript, Dart, Segmentação...</a></h2>
+					</article>
+					<footer>
+					 por: <span><a href="/quem-somos/#alex">Alex Alonso</a></span>
+					</footer>
+				</div>
+			</li>
+
+		</ul>
+
 		<div class="col-lg-4 post">
 
-			
+
 		</div>
 
 
