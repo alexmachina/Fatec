9ae3439cd2095f7646a09093e1540d8723862ad4
Alex Alonso <alex.xmde@gmail.com>
2016-04-27 18:25:08 -0300

    Listagem de posts Js melhorada

--- html/areas/javascript/index.php
+++ html/areas/javascript/index.php
@@ -17,92 +17,122 @@
 	</div>
 	<div class="separator"></div>
 	<div class="row">
-		<ul class="articles-list">
+		<ul class="articles-list js">
 			<li class="item js col-lg-3 col-md-4 col-sm-6">
 				<div class="item-container">
-					<header class="item-header">
+					<header class="item-header hidden-xs">
 						<h1><a href="#">JavaScript</a></h1>
 
 
 					</header>
 
-					<article class="item-title">
+					<article class="item-title col-xs-10 col-sm-12">
 						<h2><a href="#">Android 6.1 - O que mudou? O que mudará?
 								E mais um titulo que é muito grande e ficará escondido
 							</a>
 						</h2>
+
 					</article>
-					<footer>
+					<div class="col-xs-2 col-sm-12 post-arrow visible-xs">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</div>
+					<footer class="hidden-xs col-sm-12">
 						por: <span><a href="/quem-somos/#eclair">Eclair Da Galera</a></span>
 					</footer>
 				</div>
 			</li>
 			<li class="item js col-lg-3 col-md-4 col-sm-6">
 				<div class="item-container">
-					<header class="item-header">
+					<header class="item-header hidden-xs">
 						<h1><a href="#">JavaScript</a></h1>
-					</header>
-					<article class="item-title">
-						<h2><a href="#">ES2015 - Um novo caminho para o JavaScript</a></h2>
-					</article>
-					<footer>
-						por: <span><a href="/quem-somos/#alex">Alex Alonso</a></span>
-					</footer>
-				</div>
-			</li>
-			<li class="item js col-lg-3 col-md-4 col-sm-6">
-				<div class="item-container">
-					<header class="item-header">
-						<h1><a href="#">JavaScript</a></h1>
-					</header>
-					<article class="item-title">
-						<h2><a href="#">(Tutorial) Game em HTML5 utilizando Canvas</a></h2>
-					</article>
-					<footer>
-						por: <span><a href="/quem-somos/#alex">Guilherme Felipe</a></span>
-					</footer>
-				</div>
-			</li>
-			<li class="item js col-lg-3 col-md-4 col-sm-6">
-				<div class="item-container">
-					<header class="item-header">
-						<h1><a href="#">JavaScript</a></h1>
-					</header>
-					<article class="item-title">
-						<h2><a href="#">(Artigo) Entenda de uma vez por todas o que é UX</a></h2>
-					</article>
-					<footer>
-						por: <span><a href="/quem-somos/#amanda">Amanda Altava</a></span>
-					</footer>
-				</div>
-			</li>
-			<li class="item js col-lg-3 col-md-4 col-sm-6">
-				<div class="item-container">
-					<header class="item-header">
-						<h1><a href="#">JavaScript</a></h1>
-					</header>
-					<article class="item-title">
-						<h2><a href="#">Flat vs Realismo</a></h2>
-					</article>
-					<footer>
-						por: <span><a href="/quem-somos/#amanda">Amanda Altava</a></span>
-					</footer>
-				</div>
-			</li>
-			<li class="item js col-lg-3 col-md-4 col-sm-6">
-				<div class="item-container">
-					<header class="item-header">
-						<h1><a href="#">JavaScript</a></h1>
-					</header>
-					<article class="item-title">
-						<h2><a href="#">TypeScript, CoffeeScript, Dart, Segmentação...</a></h2>
-					</article>
-					<footer>
-						por: <span><a href="/quem-somos/#alex">Alex Alonso</a></span>
-					</footer>
-				</div>
-			</li>
 
+
+					</header>
+
+					<article class="item-title col-xs-10 col-sm-12">
+						<h2><a href="#">Android 6.1 - O que mudou? O que mudará?
+								E mais um titulo que é muito grande e ficará escondido
+							</a>
+						</h2>
+
+					</article>
+					<div class="col-xs-2 col-sm-12 post-arrow visible-xs">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</div>
+					<footer class="hidden-xs col-sm-12">
+						por: <span><a href="/quem-somos/#eclair">Eclair Da Galera</a></span>
+					</footer>
+				</div>
+			</li>
+			<li class="item js col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header hidden-xs">
+						<h1><a href="#">JavaScript</a></h1>
+
+
+					</header>
+
+					<article class="item-title col-xs-10 col-sm-12">
+						<h2><a href="#">Android 6.1 - O que mudou? O que mudará?
+								E mais um titulo que é muito grande e ficará escondido
+							</a>
+						</h2>
+
+					</article>
+					<div class="col-xs-2 col-sm-12 post-arrow visible-xs">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</div>
+					<footer class="hidden-xs col-sm-12">
+						por: <span><a href="/quem-somos/#eclair">Eclair Da Galera</a></span>
+					</footer>
+				</div>
+			</li>
+<li class="item js col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header hidden-xs">
+						<h1><a href="#">JavaScript</a></h1>
+
+
+					</header>
+
+					<article class="item-title col-xs-10 col-sm-12">
+						<h2><a href="#">Android 6.1 - O que mudou? O que mudará?
+								E mais um titulo que é muito grande e ficará escondido
+							</a>
+						</h2>
+
+					</article>
+					<div class="col-xs-2 col-sm-12 post-arrow visible-xs">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</div>
+					<footer class="hidden-xs col-sm-12">
+						por: <span><a href="/quem-somos/#eclair">Eclair Da Galera</a></span>
+					</footer>
+				</div>
+			</li>
+<li class="item js col-lg-3 col-md-4 col-sm-6">
+				<div class="item-container">
+					<header class="item-header hidden-xs">
+						<h1><a href="#">JavaScript</a></h1>
+
+
+					</header>
+
+					<article class="item-title col-xs-10 col-sm-12">
+						<h2><a href="#">Android 6.1 - O que mudou? O que mudará?
+								E mais um titulo que é muito grande e ficará escondido
+							</a>
+						</h2>
+
+					</article>
+					<div class="col-xs-2 col-sm-12 post-arrow visible-xs">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</div>
+					<footer class="hidden-xs col-sm-12">
+						por: <span><a href="/quem-somos/#eclair">Eclair Da Galera</a></span>
+					</footer>
+				</div>
+			</li>
 		</ul>
 
 		<div class="col-lg-4 post">
