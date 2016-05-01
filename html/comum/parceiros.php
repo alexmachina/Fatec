45037c51cb3ee64e8a676f135d835a771ea93bd0
Alex Alonso <alex.xmde@gmail.com>
2016-04-30 22:40:19 -0300

    Pagina parceiros no tamanho Md concertada

--- html/comum/parceiros.php
+++ html/comum/parceiros.php
@@ -11,41 +11,44 @@
 
 <hr/>
 
+<div class="row">
 <article class="article-parceiros col-xs-12 col-md-6">
 <div class="col-xs-12 col-md-6">
 	<img src="../img/gnu.png" class="img img-responsive"> 
 </div>
 
 <div class="col-xs-12 col-md-6 ">
-<h2>GNU Foundation</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
+	<h2>GNU Foundation</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
 </div>
 
 
 
 </article>
 <article class="article-parceiros col-xs-12 col-md-6">
-<div class="col-xs-12 col-md-6">
-	<img src="../img/github.png" class="img img-responsive"> 
-</div>
+	<div class="col-xs-12 col-md-6">
+		<img src="../img/github.png" class="img img-responsive"> 
+	</div>
 
-<div class="col-xs-12 col-md-6 ">
-<h2>GitHub</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
-</div>
+	<div class="col-xs-12 col-md-6 ">
+		<h2>GitHub</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
+	</div>
 
 
 
 </article>
+</div>
+<div class="row">
 <article class="article-parceiros col-xs-12 col-md-6">
-<div class="col-xs-12 col-md-6">
-	<img src="../img/w3c.png" class="img img-responsive"> 
-</div>
+	<div class="col-xs-12 col-md-6">
+		<img src="../img/w3c.png" class="img img-responsive"> 
+	</div>
 
-<div class="col-xs-12 col-md-6 ">
-<h2>W3C</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
-</div>
+	<div class="col-xs-12 col-md-6 ">
+		<h2>W3C</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
+	</div>
 
 
 
@@ -53,18 +56,19 @@
 
 
 <article class="article-parceiros col-xs-12 col-md-6">
-<div class="col-xs-12 col-md-6">
-	<img src="../img/linux.png" class="img img-responsive"> 
-</div>
+	<div class="col-xs-12 col-md-6">
+		<img src="../img/linux.png" class="img img-responsive"> 
+	</div>
 
-<div class="col-xs-12 col-md-6 ">
-<h2>Linux Foundation</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
-</div>
+	<div class="col-xs-12 col-md-6 ">
+		<h2>Linux Foundation</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor facilisis nisl imperdiet dictum. Aenean dignissim, lorem at volutpat ullamcorper, nibh tortor volutpat augue, posuere aliquet nisl magna id nunc. Integer sollicitudin gravida tortor vel mattis. Proin id pharetra dui. Mauris imperdiet dui at sodales porta. Praesent luctus turpis eu ultrices ullamcorper. In hac habitasse platea dictumst. Nunc vulputate sodales eros et pulvinar. Proin in felis a justo placerat scelerisque.</p>
+	</div>
 
 
 
 </article>
+</div>
 
 
 </div>
