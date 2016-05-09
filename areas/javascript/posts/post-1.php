0196634e6c11ddce14e209ad828e2da36659f7db
Alex Alonso <alex.xmde@gmail.com>
2016-05-09 19:06:52 -0300

    Alguma coisa

--- areas/javascript/posts/post-1.php
+++ areas/javascript/posts/post-1.php
@@ -2,31 +2,51 @@
 <article class="container">
 	<div class="row">
 
-		<div class="banner-post js col-xs-12" >
-			<div class="row">
+		<div class="banner js col-xs-12">
+
+			<div class="banner-post js col-xs-12" >
+				<div class="row">
 
 
-				<div class="title-post js col-xs-12 col-md-12">
-					<h3>ES6 - O que há de novo? O guia definitivo</h3>
-				</div>
-			</div>
-			<div class="row">
-
-
-				<div class="author-post col-md-6 col-xs-12">
-					<div class="thumb-post-container col-xs-12 col-sm-6">
-						<img class="img img-responsive" src="<?php echo $path; ?>img/thumbs/alex.jpg"/>
-					</div>
-					<div class="author-post-name-container col-xs-6 col-sm-6">
-						<p>Alex Alonso</p>
-						<p>
-							Autor
-						</p>
-
+					<div class="title-post js col-xs-12">
+						<h3>ES6 - O que há de novo? O guia definitivo</h3>
 					</div>
 
 				</div>
+				<div class="row">
+					<div class="author-post col-xs-8">
+						<div class="thumb-post-container col-xs-12 ">
+							<img class="thumb" src="<?php echo $path; ?>/img/thumbs/alex.png"/>
+						</div>
+						<div class="author-post-name-container col-xs-6">
+							<p class="author-name"><a href="/comum/quem-somos.php"> Alex Alonso</a></p>
+							<p class="author">
+								Autor
+							</p>
+
+						</div>
+						<div id="circulo-post" class="col-xs-4 hidden-xs">
+						Js
+					</div>
+
+					</div>
+
+				</div>
 			</div>
+			<!-- CIRCULO
+			<div id="" class="col-sm-1 hidden-xs circulo-tag-2">
+				Js
+			</div>
+			<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-3">
+				ES6
+			</div>
+			<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-4">
+				Guia
+			</div>
+			<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-4">
+				Novo
+			</div>
+		-->
 		</div>
 	</div>
 
