0b391e58ef661b4803c59d46aae7c010a85c421e
Alex Alonso <alex.xmde@gmail.com>
2016-05-11 20:12:42 -0300

    Logo adicionado

--- areas/javascript/posts/post-1.php
+++ areas/javascript/posts/post-1.php
@@ -26,8 +26,8 @@
 
 						</div>
 						<div id="circulo-post" class="col-xs-4 col-xs-offset-1 col-sm-offset-0">
-						Js
-					</div>
+							Js
+						</div>
 
 					</div>
 
@@ -35,20 +35,30 @@
 			</div>
 			<!-- CIRCULO
 			<div id="" class="col-sm-1 hidden-xs circulo-tag-2">
-				Js
-			</div>
-			<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-3">
-				ES6
-			</div>
-			<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-4">
-				Guia
-			</div>
-			<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-4">
-				Novo
-			</div>
-		-->
+			Js
 		</div>
+		<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-3">
+		ES6
 	</div>
+	<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-4">
+	Guia
+</div>
+<div id="circulo-tag" class="col-sm-1 hidden-xs circulo-tag-4">
+Novo
+</div>
+-->
+</div>
+</div>
+<div class="row">
+
+	<!-- Artigo -->
+	<div class="col-xs-12 col-sm-9 js-article-1">
+
+	</div>
+	<div class="col-xs-12 col-sm-3 js-article-1-related">
+
+	</div>
+</div>
 
 </article>
 <?php include '../../../templates/footer.html'; ?>
